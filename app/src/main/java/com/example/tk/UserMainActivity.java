e29d651eb2f19037b0591a0bc2e1a4b1ec309cc2
lenovo <3366209176@qq.com>
2025-06-25 15:29:10 +0800

    `main2user`

---
+++ app/src/main/java/com/example/tk/UserMainActivity.java
@@ -0,0 +1,16 @@
+package com.example.tk;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserMainActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_layout);
+
+
+    }
+}
