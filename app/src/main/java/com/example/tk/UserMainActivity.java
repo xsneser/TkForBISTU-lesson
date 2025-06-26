247c3e3ba436f004c794bb810813c612767bbe5d
lenovo <3366209176@qq.com>
2025-06-26 10:11:06 +0800

    `xnseser`

--- app/src/main/java/com/example/tk/UserMainActivity.java
+++ app/src/main/java/com/example/tk/UserMainActivity.java
@@ -2,7 +2,10 @@ package com.example.tk;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class UserMainActivity extends AppCompatActivity{
 
@@ -10,7 +13,16 @@ public class UserMainActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_layout);
-
+        LinearLayout meView = findViewById(R.id.linearLayout4);
+        meView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(UserMainActivity.this, UserMainMeActivity.class);
+                startActivity(intent);
+                //overridePendingTransition(0, 0);
+                //finish();
+            }
+        });
 
     }
 }
--- app/src/main/java/com/example/tk/UserMainMeActivity.java
+++ app/src/main/java/com/example/tk/UserMainMeActivity.java
@@ -1,6 +1,9 @@
 package com.example.tk;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,7 +12,7 @@ public class UserMainMeActivity extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_layout);
+        setContentView(R.layout.main_me_layout);
 
 
     }
