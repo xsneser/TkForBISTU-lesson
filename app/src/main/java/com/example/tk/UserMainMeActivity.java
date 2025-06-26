aeb477e568ac0567d077fd6599b6abda7e37a537
lenovo <3366209176@qq.com>
2025-06-26 09:29:53 +0800

    `xnseser`

---
+++ app/src/main/java/com/example/tk/UserMainMeActivity.java
@@ -0,0 +1,16 @@
+package com.example.tk;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class UserMainMeActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_layout);
+
+
+    }
+}
