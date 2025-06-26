7242941815c09fccf726f9839f0de2142d602820
lenovo <3366209176@qq.com>
2025-06-26 10:25:54 +0800

    `xnseser`

--- app/src/main/java/com/example/tk/UserMainActivity.java
+++ app/src/main/java/com/example/tk/UserMainActivity.java
@@ -13,6 +13,28 @@ public class UserMainActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_layout);
+
+        LinearLayout messageView = findViewById(R.id.linearLayout2);
+        messageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(UserMainActivity.this, UserMainMessageActivity.class);
+                startActivity(intent);
+                overridePendingTransition(0, 0);
+                //finish();
+            }
+        });
+
+        LinearLayout newView = findViewById(R.id.linearLayout3);
+        newView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(UserMainActivity.this, UserMainNewActivity.class);
+                startActivity(intent);
+                overridePendingTransition(0, 0);
+                //finish();
+            }
+        });
         LinearLayout meView = findViewById(R.id.linearLayout4);
         meView.setOnClickListener(new View.OnClickListener() {
             @Override
